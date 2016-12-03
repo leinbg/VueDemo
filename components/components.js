8e6f9a470c63adcd92e505779f801c096f0c5f0e
leinbg <leiw4github@gmail.com>
2016-12-03 23:59:58 +0100

    tab component in index

--- components/components.js
+++ components/components.js
@@ -36,8 +36,65 @@ Vue.component('message', {
 	}
 });
 
+// Subheader Component
 Vue.component('subheader', {
 	template: `
 		<h3 class="subtitle"><slot></slot></h3>
 	`
+});
+
+// tabs component
+Vue.component('tabs', {
+	template: `
+		<div>
+			<div class="tabs">
+				<ul>
+					<li v-for="tab in tabs" :class="{'is-active': tab.isActive}">
+						<a :href="tab.link" @click="selectTab(tab)">{{ tab.name }}</a>
+					</li>
+				</ul>
+			</div>
+			<div class="tabs-details">
+				<slot></slot>
+			</div>
+		</div>
+	`,
+	methods: {
+		selectTab(selectedTab) {
+			this.tabs.forEach(function(tab){
+				tab.isActive = (tab.name == selectedTab.name);
+			})
+		}
+	},
+	data() {
+		return {
+			tabs: []
+		};
+	},
+	created() {
+		this.tabs = this.$children;
+	}
+});
+
+Vue.component('tab', {
+	template: `
+		<div v-show="isActive"><slot></slot></div>
+	`,
+	props: {
+		name: {required: true},
+		selected: {default: false}
+	},
+	computed: {
+		link() {
+			return '#' + this.name.toLowerCase().replace(/ /g, '-');
+		}
+	},
+	data() {
+		return {
+			isActive: false
+		};
+	},
+	mounted() {
+		this.isActive = this.selected;
+	}
 });
